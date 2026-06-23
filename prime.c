c349fae5e26d0523e0bc182a4ab2bddf366481b5
Vedashree Balu Aher <ahervedashree0@gmail.com>
2026-06-23 19:14:23 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int n, i, flag = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n <= 1) flag = 0;
+    for(i = 2; i < n; i++) {
+        if(n % i == 0) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Prime number");
+    else
+        printf("Not prime number");
+
+    return 0;
+}
